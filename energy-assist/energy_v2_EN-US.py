faae4d7dea9dcaed7ccf0a02db48edb7061318b6
mthsl <83477899+mthsl@users.noreply.github.com>
2023-11-29 19:59:09 -0300

    Add files via upload

---
+++ energy-assist/energy_v2_EN-US.py
@@ -0,0 +1,50 @@
+seeds = {
+    1: {'name': 'Popberry', 'energy_cost': 3.5},
+    2: {'name': 'Butterberry', 'energy_cost': 3.5},
+    3: {'name': 'Clover', 'energy_cost': 5},
+    4: {'name': 'Grainbow', 'energy_cost': 4},
+    5: {'name': 'Watermint', 'energy_cost': 8},
+    6: {'name': 'Scarrot', 'energy_cost': 8}
+}
+
+print('ENERGY CALCULATION SYSTEM: Check if it is possible to plant, water, and harvest based on your current energy amount!')
+print('MENU:', ', '.join(f'{key} - {seed["name"]}' for key, seed in seeds.items()))
+print()
+
+choice = int(input('Enter your choice according to the options above: '))
+current_energy = float(input('Enter your current energy amount: '))
+
+watering_cost = 0.5
+harvesting_cost = 1
+
+def operation_text(operation):
+    operations = {1: 'plant', 2: 'water', 3: 'harvest', 4: 'plant and water', 5: 'plant, water, and harvest'}
+    return operations.get(operation, 'Option not found!')
+
+if choice in seeds:
+    seed = seeds[choice]
+
+    print(' ')
+    print('What do you want to do?')
+    print('1 - Plant, 2 - Water, 3 - Harvest, 4 - Plant and Water, 5 - Plant, Water, and Harvest')
+    print(' ')
+
+    operation_choice = int(input('Enter your choice: '))
+
+    if operation_choice in [1, 2, 3, 4, 5]:
+        if operation_choice == 1:
+            max_seeds_possible = int(current_energy // seed['energy_cost'])
+        elif operation_choice == 2:
+            max_seeds_possible = int(current_energy // watering_cost)
+        elif operation_choice == 3:
+            max_seeds_possible = int(current_energy // harvesting_cost)
+        elif operation_choice == 4:
+            max_seeds_possible = int(current_energy // (seed['energy_cost'] + watering_cost))
+        elif operation_choice == 5:
+            max_seeds_possible = int(current_energy // (seed['energy_cost'] + watering_cost + harvesting_cost))
+            
+        print(f'With the current amount of energy, you can {operation_text(operation_choice)} a maximum of {max_seeds_possible} {seed["name"]}(s).')
+    else:
+        print('Option not found!')
+else:
+    print('Option not found!')
---
+++ energy-assist/energy_v2_PT-BR.py
@@ -0,0 +1,50 @@
+seeds = {
+    1: {'nome': 'Popberry', 'energia_gasta': 3.5},
+    2: {'nome': 'Butterberry', 'energia_gasta': 3.5},
+    3: {'nome': 'Clover', 'energia_gasta': 5},
+    4: {'nome': 'Grainbow', 'energia_gasta': 4},
+    5: {'nome': 'Watermint', 'energia_gasta': 8},
+    6: {'nome': 'Scarrot', 'energia_gasta': 8}
+}
+
+print('SISTEMA PARA CÁLCULO DE GASTO DE ENERGIA: Calcule se é possível plantar, regar e colher de acordo com a sua quantidade de energia atual!')
+print('MENU:', ', '.join(f'{key} - {seed["nome"]}' for key, seed in seeds.items()))
+print()
+
+escolha = int(input('Digite sua escolha de acordo com as opções acima: '))
+qtd_energia = float(input('Digite a quantidade de energia atual: '))
+
+regar = 0.5
+colher = 1
+
+def operacao_texto(operacao):
+    operacoes = {1: 'plantar', 2: 'regar', 3: 'colher', 4: 'plantar e regar', 5: 'plantar, regar e colher'}
+    return operacoes.get(operacao, 'Opção não encontrada!')
+
+if escolha in seeds:
+    seed = seeds[escolha]
+
+    print(' ')
+    print('O que você deseja fazer?')
+    print('1 - Plantar, 2 - Regar, 3 - Colher, 4 - Plantar e Regar, 5 - Plantar, Regar e Colher (Todas as ações)')
+    print(' ')
+
+    escolha_operacao = int(input('Digite sua escolha: '))
+
+    if escolha_operacao in [1, 2, 3, 4, 5]:
+        if escolha_operacao == 1:
+            qtd_seeds_possivel = int(qtd_energia // seed['energia_gasta'])
+        elif escolha_operacao == 2:
+            qtd_seeds_possivel = int(qtd_energia // regar)
+        elif escolha_operacao == 3:
+            qtd_seeds_possivel = int(qtd_energia // colher)
+        elif escolha_operacao == 4:
+            qtd_seeds_possivel = int(qtd_energia // (seed['energia_gasta'] + regar))
+        elif escolha_operacao == 5:
+            qtd_seeds_possivel = int(qtd_energia // (seed['energia_gasta'] + regar + colher))
+            
+        print(f'Com a quantidade de energia atual, você pode {operacao_texto(escolha_operacao)} no máximo {qtd_seeds_possivel} {seed["nome"]}(s).')
+    else:
+        print('Opção não encontrada!')
+else:
+    print('Opção não encontrada!')
